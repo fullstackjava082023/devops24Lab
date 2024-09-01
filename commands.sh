c7659d0c37bc4549e0d1774789ad5f79e8cb9025
tamir <tamirb771@gmail.com>
2024-09-01 19:47:12 +0300

    First Commit

---
+++ commands.sh
@@ -0,0 +1,12 @@
+# Description: This file contains the commands that I have learned in the git course.
+git init
+
+# This command is used to check the status of the files in the repository.
+git status
+
+# This command is used to add files to the staging area.
+git add 
+
+# to add files to git repository.
+git add commands.sh
+
